ea4b1ac08f242c739961ee847c23e4869519aa94
Szymon Miężał <szymon.miezal@student.put.poznan.pl>
2017-06-26 13:18:24 +0200

    config.

--- start-benchmark.sh
+++ start-benchmark.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar target/DURs-1.0-SNAPSHOT.jar -port $1 -address $2 -B -keys 300 -threads 320 -readsInQuery 100 -readsInUpdate 8 -writesInUpdate 2 -delay 0 -saturation 1
+java -jar target/DURs-1.0-SNAPSHOT.jar -port $1 -address $2 -B -keys 300 -threads 4 -readsInQuery 100 -readsInUpdate 8 -writesInUpdate 2 -delay 0 -saturation 1
