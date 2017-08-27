e6a3d77247c4e57425de490b25ad4c3d5e91593e
Szymon Miężał <szymon.miezal@student.put.poznan.pl>
2017-08-27 17:24:17 +0200

    start script fix.

--- start-replica.sh
+++ start-replica.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 rm -rf jpaxosLogs
-rm -f $7
 
 java -Dserver.port=$((8090 + $1)) -jar target/DURs-1.0-SNAPSHOT.jar -id $1 -port $2 -paxosProcesses $3 -bossThreads $4 -workerThreads $5 --spring.profiles.active=$6 > $7 2>&1
