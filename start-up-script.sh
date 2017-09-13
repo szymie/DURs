38d4735fa5ea6eb94af341a98f41131eaa92014a
Szymon Miężał <szymon.miezal@student.put.poznan.pl>
2017-09-14 00:31:40 +0200

    script update.

--- start-up-script.sh
+++ start-up-script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+CLIENT_POOL_SIZE=$1
+shift
+WORKER_THREADS=$1
+shift
 PROFILE=$1
 shift
 PAXOS_PROCESSES=$1
@@ -15,7 +19,7 @@ echo $ID_COUNTER
 
 rm -rf jpaxosLogs
 
-ssh -o StrictHostKeyChecking=no -n -f inf109733@$1 "sh -c 'cd /home/inf109733/mgr/DURs; nohup java -Dserver.port=8090 -jar target/DURs-1.0-SNAPSHOT.jar -id $ID_COUNTER -port 8080 -paxosProcesses $PAXOS_PROCESSES -bossThreads 0 -workerThreads 0 --spring.profiles.active=$PROFILE > /dev/null 2>&1 &'"
+ssh -o StrictHostKeyChecking=no -n -f inf109733@$1 "sh -c 'cd /home/inf109733/mgr/DURs; nohup java -Xmx3g -Dserver.port=8090 -jar target/DURs-1.0-SNAPSHOT.jar -id $ID_COUNTER -port 8080 -paxosProcesses $PAXOS_PROCESSES -clientPoolSize $CLIENT_POOL_SIZE -bossThreads 0 -workerThreads $WORKER_THREADS --spring.profiles.active=$PROFILE > $ID_COUNTER 2>&1 &'"
 ssh -o StrictHostKeyChecking=no -n -f inf109733@$1 "sh -c 'cd /home/inf109733/mgr/DURs; nohup ../apache-jmeter-3.2/bin/jmeter-server -Djava.rmi.server.hostname=10.10.0.$FIRST_IP > /dev/null 2>&1 &'"	
 
 FIRST_IP=$((FIRST_IP+1))
